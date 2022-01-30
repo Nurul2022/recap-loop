aea00613459630e1c1c5f688f14d6c938499dcdc
Nurul2022 <nurulbdpersonal@gmail.com>
2022-01-30 18:24:27 +0600

    while loop example added

---
+++ while-loop-example.js
@@ -0,0 +1,18 @@
+// var number = 0; // loop variable
+// while (number <= 20) {
+//     console.log(number);
+//     number++;
+// }
+
+// odd number (bijor songkha)
+// var number = 1; // loop variable
+// while (number <= 20) {
+//     console.log(number);
+//     number = number +2;
+// }
+// even number (jor songkha)
+var number = 0; // loop variable
+while (number <= 20) {
+    console.log(number);
+    number = number +2;
+}
